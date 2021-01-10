caaff04aae8e04660a4efe32296609c95e8c480c
Roy Riojas <royriojas@gmail.com>
2021-01-09 22:27:27 -0800

    REF: Upgrade deps

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import './App.css';
-import { Counter } from './components/Counter';
 import { LoginForm } from './components/LoginForm';
 
 function App() {
   return (
     <div className="App">
-      <Counter />
       <LoginForm />
     </div>
   );
